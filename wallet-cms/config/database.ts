fdec89c43bc3c89eeeed96bea5d2334389174d28
jp34 <joekadlic@outlook.com>
2022-12-20 15:28:46 -0500

    Updated docker configuration

--- wallet-cms/config/database.ts
+++ wallet-cms/config/database.ts
@@ -1,17 +1,26 @@
 require('dotenv').config();
 
-export default ({ env }) => ({
-	connection: {
-		client: 'postgres',
+export default ({ env }) => {
+	strapi.log.info(`DATABASE_HOST: ${process.env.DB_HOST}`);
+	strapi.log.info(`DATABASE_PORT: ${process.env.DB_PORT}`);
+	strapi.log.info(`DATABASE_NAME: ${process.env.DB_NAME}`);
+	strapi.log.info(`DATABASE_USERNAME: ${process.env.DB_USER}`);
+	strapi.log.info(`DATABASE_PASSWORD: ${process.env.DB_PASSWORD}`);
+	strapi.log.info(`DATABASE_SCHEMA: ${process.env.DB_SCHEMA}`);
+	
+	return {
 		connection: {
-			host: env('DATABASE_HOST', process.env.DB_HOST),
-			port: env.int('DATABASE_PORT', process.env.DB_PORT),
-			database: env('DATABASE_NAME', process.env.DB_NAME),
-			user: env('DATABASE_USERNAME', process.env.DB_USER),
-			password: env('DATABASE_PASSWORD', process.env.DB_PASS),
-			schema: env('DATABASE_SCHEMA', process.env.DB_SCHEMA),
-			ssl: env('DATABASE_SSL', false)
+			client: 'postgres',
+			connection: {
+				host: env('DATABASE_HOST', process.env.DB_HOST),
+				port: env.int('DATABASE_PORT', process.env.DB_PORT),
+				database: env('DATABASE_NAME', process.env.DB_NAME),
+				user: env('DATABASE_USERNAME', process.env.DB_USER),
+				password: env('DATABASE_PASSWORD', process.env.DB_PASSWORD),
+				schema: env('DATABASE_SCHEMA', process.env.DB_SCHEMA),
+				ssl: env('DATABASE_SSL', false)
+			},
+			debug: false,
 		},
-		debug: false,
-	},
-});
+	}
+};
--- wallet-cms/config/server.ts
+++ wallet-cms/config/server.ts
@@ -1,11 +1,16 @@
 require('dotenv').config();
 
 module.exports = ({ env }) => {
-	const host = env('HOST', process.env.SERVER_HOST);
-	const port = env.int('PORT', process.env.SERVER_PORT);
-	const url = env('URL', process.env.SERVER_URL);
+	strapi.log.info(`SERVER_HOST: ${process.env.SERVER_HOST}`);
+	strapi.log.info(`SERVER_PORT: ${process.env.SERVER_PORT}`);
+	strapi.log.info(`SERVER_URL: ${process.env.SERVER_URL}`);
+	strapi.log.info(`SERVER_KEY_1: ${process.env.SERVER_KEY_1}`);
+	strapi.log.info(`SERVER_KEY_2: ${process.env.SERVER_KEY_2}`);
+
 	return {
-		host, port, url,
+		host: env('HOST', process.env.SERVER_HOST),
+		port: env.int('PORT', process.env.SERVER_PORT),
+		url: env('URL', process.env.SERVER_URL),
 		app: {
 			keys: env.array('APP_KEYS', [process.env.SERVER_KEY_1, process.env.SERVER_KEY_2]),
 		},

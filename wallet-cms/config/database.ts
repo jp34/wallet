3857aa15c35b11b8b3c7f1b65e3fd916c341d665
jp34 <joekadlic@outlook.com>
2022-12-29 12:29:39 -0500

    Updated Strapi connection string builder

--- wallet-cms/config/database.ts
+++ wallet-cms/config/database.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 export default ({ env }) => {
+	let user = env('DATABASE_USER', process.env.DATABASE_USER);
+	let pass = env('DATABASE_PASS', process.env.DATABASE_PASS);
 	let host = env('DATABASE_HOST', process.env.DATABASE_HOST);
 	let port = env('DATABASE_PORT', process.env.DATABASE_PORT);
 	let database = env('DATABASE_NAME', process.env.DATABASE_NAME);
@@ -9,7 +11,7 @@ export default ({ env }) => {
 		connection: {
 			client: 'postgres',
 			connection: {
-				connectionString: `postgres://${host}:${port}/${database}`,
+				connectionString: `postgres://${user}:${pass}@${host}:${port}/${database}`,
 				ssl: env('DATABASE_SSL', false)
 			},
 			debug: false,

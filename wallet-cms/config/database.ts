0fd864ccf3912973de51ebc0f208e0b19816fba4
jp34 <joekadlic@outlook.com>
2022-12-29 11:45:14 -0500

    Updated docker-compose.yml

--- wallet-cms/config/database.ts
+++ wallet-cms/config/database.ts
@@ -1,23 +1,15 @@
 require('dotenv').config();
 
 export default ({ env }) => {
-	console.log(`DATABASE_HOST: ${process.env.DB_HOST}`);
-	console.log(`DATABASE_PORT: ${process.env.DB_PORT}`);
-	console.log(`DATABASE_NAME: ${process.env.DB_NAME}`);
-	console.log(`DATABASE_USERNAME: ${process.env.DB_USER}`);
-	console.log(`DATABASE_PASSWORD: ${process.env.DB_PASS}`);
-	console.log(`DATABASE_SCHEMA: ${process.env.DB_SCHEMA}`);
+	let host = env('DATABASE_HOST', process.env.DATABASE_HOST);
+	let port = env('DATABASE_PORT', process.env.DATABASE_PORT);
+	let database = env('DATABASE_NAME', process.env.DATABASE_NAME);
 	
 	return {
 		connection: {
 			client: 'postgres',
 			connection: {
-				host: env('DATABASE_HOST', process.env.DB_HOST),
-				port: env.int('DATABASE_PORT', process.env.DB_PORT),
-				database: env('DATABASE_NAME', process.env.DB_NAME),
-				user: env('DATABASE_USERNAME', process.env.DB_USER),
-				password: env('DATABASE_PASSWORD', process.env.DB_PASS),
-				schema: env('DATABASE_SCHEMA', process.env.DB_SCHEMA),
+				connectionString: `postgres://${host}:${port}/${database}`,
 				ssl: env('DATABASE_SSL', false)
 			},
 			debug: false,
--- wallet-cms/config/server.ts
+++ wallet-cms/config/server.ts
@@ -1,18 +1,12 @@
 require('dotenv').config();
 
 module.exports = ({ env }) => {
-	console.log(`SERVER_HOST: ${process.env.SERVER_HOST}`);
-	console.log(`SERVER_PORT: ${process.env.SERVER_PORT}`);
-	console.log(`SERVER_URL: ${process.env.SERVER_URL}`);
-	console.log(`SERVER_KEY_1: ${process.env.SERVER_KEY_1}`);
-	console.log(`SERVER_KEY_2: ${process.env.SERVER_KEY_2}`);
-
 	return {
 		host: env('HOST', process.env.SERVER_HOST),
 		port: env.int('PORT', process.env.SERVER_PORT),
 		url: env('URL', process.env.SERVER_URL),
 		app: {
-			keys: env.array('APP_KEYS', [process.env.SERVER_KEY_1, process.env.SERVER_KEY_2]),
+			keys: env.array('APP_KEYS', [process.env.SERVER_APP_KEY_1, process.env.SERVER_APP_KEY_2]),
 		},
 	}
 }

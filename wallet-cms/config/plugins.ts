648b307be5d0b7a1ce46470c8422932541c58827
jp34 <joekadlic@outlook.com>
2023-02-02 17:06:34 -0500

    Added data-type visualizer plugin

--- wallet-cms/config/plugins.ts
+++ wallet-cms/config/plugins.ts
@@ -6,6 +6,23 @@ export default ({ env }) => {
             config: {
                 jwtSecret: env('STRAPI_SECRET' || crypto.randomBytes(15).toString('base64'))
             }
-        }
+        },
+        "entity-relationship-chart": {
+            enabled: true,
+            config: {
+                exclude: [
+                    "strapi::core-store",
+                    "webhook",
+                    "admin::permission",
+                    "admin::user",
+                    "admin::role",
+                    "admin::api-token",
+                    "plugin::upload.file",
+                    "plugin::i18n.locale",
+                    "plugin::users-permissions.permission",
+                    "plugin::users-permissions.role",
+                ],
+            },
+        },
 	}
 };

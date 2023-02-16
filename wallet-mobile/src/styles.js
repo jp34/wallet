ab2f7090a103ae0c6c8b5764858f12645e9d0745
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-16 16:48:17 -0500

    Added styles.js, altering Login.js, removed Homescreen.js

---
+++ wallet-mobile/src/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  colors: {
+    primary: '#6030D9',
+    secondary: '#2B1360',
+    gradient1: ['#2B1360', '#6030D9'],
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+  },
+  text: {
+    title: {
+      color: '#FFF',
+      fontSize: 35,
+      fontWeight: 'bold',
+      marginVertical: 30,
+    },
+  },
+});
+
+export default styles;

950b7a09bc308d36dd783c23c1e57158e53223f7
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-16 16:29:33 -0500

    Create Logo.js

---
+++ wallet-mobile/src/components/Logo.js
@@ -0,0 +1,29 @@
+import { Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const Logo = () => {
+  return (
+    <LinearGradient style={styles.linearGradient} colors={['#6030D9', '#2B1360']}>
+      <Text style={styles.text}>JustBe</Text>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    width: 180,
+    height: 180,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    alignSelf: 'center',
+  },
+  text: {
+    color: '#FFF',
+    fontSize: 35,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+  },
+});
+
+export default Logo;

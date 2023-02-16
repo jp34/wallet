ecc09051396e2fa95cddaf355f198e325dc9faec
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-16 16:17:53 -0500

    Update Button.js

--- wallet-mobile/src/components/Button.js
+++ wallet-mobile/src/components/Button.js
@@ -1,26 +1,56 @@
 import { TouchableOpacity, Text, Image, View, StyleSheet } from 'react-native';
-import styles from './styles';
 
-const Button = ({ text, imgSource, textColor, backColor }) => {
+const Button = ({ text, imgSource, textColor, backColor, onPress }) => {
   return (
-    <TouchableOpacity style={styles.buttons.view} activeOpacity={0.5}>
-      {imgSource && <Image source={imgSource} style={styles.buttons.image} />}
+    <TouchableOpacity style={styles.view} activeOpacity={0.5} onPress={onPress}>
+      {imgSource && <Image source={imgSource} style={styles.image} />}
       {imgSource && (
         <View
-          style={StyleSheet.compose(styles.buttons.separator, {
+          style={StyleSheet.compose(styles.separator, {
             backgroundColor: backColor,
           })}
         />
       )}
-      <Text
-        style={StyleSheet.compose(styles.buttons.text, {
-          color: textColor,
-        })}
-      >
+      <Text style={StyleSheet.compose(styles.text, { color: textColor })}>
         {text}
       </Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 60,
+    borderRadius: 10,
+    marginHorizontal: 30,
+    marginVertical: 5,
+    paddingHorizontal: 20,
+  },
+  image: {
+    padding: 20,
+    height: 45,
+    width: 45,
+    resizeMode: 'stretch',
+    marginRight: 15,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center',
+  },
+  separator: {
+    height: 45,
+    borderRadius: 20,
+    width: 1.25,
+  },
+  group: {
+    marginHorizontal: 30,
+    paddingVertical: 5,
+  },
+});
 
 export default Button;

c59af32bb031369227c826a70ebe06be49c03b3a
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-19 19:12:31 -0500

    Update CheckboxList.js

--- wallet-mobile/src/components/CheckboxList.js
+++ wallet-mobile/src/components/CheckboxList.js
@@ -1,50 +1,67 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity} from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const CheckboxList = ({ options }) => {
+  // Constant and component to handle checked items.
   const [checkedItems, setCheckedItems] = useState({});
-
   const handleCheckedItem = (item) => {
     setCheckedItems({ ...checkedItems, [item]: !checkedItems[item] });
   };
 
   return (
+    // Container for checkbox list.
     <View>
+      {/* Component to Create Checkbox List */}
       {options.map((item, index) => (
-        <View
-          key={index}
-          style={{ flexDirection: "row", alignItems: "center", marginVertical: 6}}
-        >
+        // Item View
+        <View key={index} style={styles.item.view}>
           <TouchableOpacity
-            style={{
-              height: 35,
-              width: 35,
-              borderWidth: 1.25,
-              borderColor: "#FFF",
-              borderRadius: 12,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
+            style={styles.checkbox.view}
             onPress={() => handleCheckedItem(item)}
           >
-            {checkedItems[item] && (
-              <View
-                style={{
-                  height: 12,
-                  width: 12,
-                  borderRadius: 6,
-                  backgroundColor: "#FFF",
-                }}
-              />
-            )}
+            {/* If Checked, Enable View */}
+            {checkedItems[item] && <View style={styles.checkbox.enabled} />}
           </TouchableOpacity>
-          <Text style={{ color: "#FFF", fontSize: 22, marginLeft: 10, marginVertical: 7, fontWeight: "475" }}>
-            {item}
-          </Text>
+          {/* Item Text */}
+          <Text style={styles.item.text}>{item}</Text>
         </View>
       ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    view: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginVertical: 6,
+    },
+    text: {
+      color: "#FFF",
+      fontSize: 22,
+      marginLeft: 10,
+      marginVertical: 7,
+      fontWeight: "475",
+    },
+  },
+  checkbox: {
+    view: {
+      height: 35,
+      width: 35,
+      borderWidth: 1.25,
+      borderColor: "#FFF",
+      borderRadius: 12,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    enabled: {
+      height: 12,
+      width: 12,
+      borderRadius: 6,
+      backgroundColor: "#FFF",
+    },
+  },
+});
 
 export default CheckboxList;

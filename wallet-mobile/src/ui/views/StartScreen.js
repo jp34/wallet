af2b452853fe4f7f13105a97f7fd4a3e588c7e85
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-21 03:55:39 -0500

    Update StartScreen.js

--- wallet-mobile/src/ui/views/StartScreen.js
+++ wallet-mobile/src/ui/views/StartScreen.js
@@ -1,11 +1,65 @@
 import React from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { ScreenStyles, Gradients } from "../Styles";
 import { LargeLogo } from "../components/Logos";
 import { PrimaryButton } from "../components/Buttons";
+import { LinearGradient } from "expo-linear-gradient";
+import {Gradients, ScreenStyles} from "../Styles";
 
 const StartScreen = ({ navigation }) => {
+  // Renders Logo
+  const renderLogo = () => {
+    return (
+      // Logo Container
+      <View style={StartStyles.logoContainer}>
+        {/* Large Logo */}
+        <LargeLogo />
+      </View>
+    );
+  };
+
+  // Renders Sign Up Button
+  const renderSignUpButton = () => {
+    return (
+      // Sign Up Button Container
+      <View style={StartStyles.buttonContainer}>
+        {/* Sign Up Button */}
+        <PrimaryButton
+          text="Get Started"
+          options={{
+            onPress: () => {
+              return navigation.navigate("Splash");
+            },
+          }}
+        />
+        {renderLoginSubtitle()}
+      </View>
+    );
+  };
+
+  // Renders Login Subtitle
+  const renderLoginSubtitle = () => {
+    return (
+      // Login Subtitle View
+      <View style={{ flexDirection: "row", marginTop: 10 }}>
+        {/* Subtitle Text */}
+        <Text style={{ color: "#EEE", fontFamily: "Quicksand-Regular" }}>
+          Already have an account?
+        </Text>
+        {/* Subtitle Hyperlink */}
+        <Text
+          style={{
+            paddingLeft: 8,
+            color: "#2A93D5",
+            fontFamily: "Quicksand-Bold",
+          }}
+          onPress={() => navigation.navigate("Login")}
+        >
+          Log In
+        </Text>
+      </View>
+    );
+  };
+
   return (
     // Background Gradient
     <LinearGradient colors={Gradients.gradient1} style={{ flex: 1 }}>
@@ -13,39 +67,9 @@ const StartScreen = ({ navigation }) => {
       <SafeAreaView style={{ flex: 1 }}>
         {/* Screen Container */}
         <View style={ScreenStyles.container}>
-          {/* Logo Container */}
-          <View style={StartStyles.logoContainer}>
-            {/* Large Logo */}
-            <LargeLogo />
-          </View>
-          {/* Form Container */}
-          <View style={StartStyles.formContainer}>
-            {/* Sign Up Button */}
-            <PrimaryButton
-              text="Get Started"
-              options={{
-                onPress: () => {
-                  return navigation.navigate("Splash");
-                },
-              }}
-            />
-            {/* Login Subtitle View */}
-            <View style={{ flexDirection: "row", marginTop: 10 }}>
-              {/* Subtitle Text */}
-              <Text style={{ color: "#EEE", fontFamily: "Quicksand-Regular" }}>Already have an account?</Text>
-              {/* Subtitle Hyperlink */}
-              <Text
-                style={{
-                  paddingLeft: 8,
-                  color: "#2A93D5",
-                  fontFamily: "Quicksand-Bold"
-                }}
-                onPress={() => navigation.navigate("Login")}
-              >
-                Log In
-              </Text>
-            </View>
-          </View>
+          {/* Render Components */}
+          {renderLogo()}
+          {renderSignUpButton()}
         </View>
       </SafeAreaView>
     </LinearGradient>
@@ -58,7 +82,7 @@ const StartStyles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  formContainer: {
+  buttonContainer: {
     flex: 0.2,
     justifyContent: "center",
     alignItems: "center",

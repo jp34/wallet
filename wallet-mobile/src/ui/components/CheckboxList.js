f063322ff7665339dd244f4c34bcdd28a1ff7a90
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-27 06:17:39 -0500

    Update CheckboxList.js

---
+++ wallet-mobile/src/ui/components/CheckboxList.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const CheckboxList = ({ options }) => {
+  const [checkedItems, setCheckedItems] = useState({});
+  const handleCheckedItem = (item) => {
+    setCheckedItems({ ...checkedItems, [item]: !checkedItems[item] });
+  };
+
+  return (
+    <View>
+      {/* Component to Create Checkbox List */}
+      {options.map((item, index) => (
+        // Item View
+        <View key={index} style={styles.item.view}>
+          <TouchableOpacity
+            style={styles.checkbox.view}
+            onPress={() => handleCheckedItem(item)}
+          >
+            {/* If Checked, Enable View */}
+            {checkedItems[item] && <View style={styles.checkbox.enabled} />}
+          </TouchableOpacity>
+          {/* Item Text */}
+          <Text style={styles.item.text}>{item}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    view: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginVertical: 6,
+    },
+    text: {
+      color: "#FFF",
+      fontSize: 18,
+      marginLeft: 10,
+      marginVertical: 7,
+      fontWeight: "475",
+    },
+  },
+  checkbox: {
+    view: {
+      height: 35,
+      width: 35,
+      borderWidth: 1.25,
+      borderColor: "#FFF",
+      borderRadius: 12,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    enabled: {
+      height: 12,
+      width: 12,
+      borderRadius: 6,
+      backgroundColor: "#FFF",
+    },
+  },
+});
+
+export default CheckboxList;

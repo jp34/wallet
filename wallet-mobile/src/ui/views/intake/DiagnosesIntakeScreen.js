fdfe3ca8a3f78ff698764050cc5ea91235a42600
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-27 00:24:19 -0500

    Create DiagnosesIntakeScreen.js

---
+++ wallet-mobile/src/ui/views/intake/DiagnosesIntakeScreen.js
@@ -0,0 +1,13 @@
+import { LinearGradient } from "expo-linear-gradient";
+
+const DiagnosesIntakeScreen = ({ navigation }) => {
+    return (
+        <LinearGradient>
+            <SafeAreaView>
+                <View>
+                    <Header />
+                </View>
+            </SafeAreaView>
+        </LinearGradient>
+    );
+};

b06fe1595ab6db654be553ea2fcc8e3a4b193c27
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-27 00:34:49 -0500

    Update DiagnosesIntakeScreen.js

--- wallet-mobile/src/ui/views/intake/DiagnosesIntakeScreen.js
+++ wallet-mobile/src/ui/views/intake/DiagnosesIntakeScreen.js
@@ -1,13 +1,18 @@
 import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView, View } from "react-native";
+import { Gradients } from "../../Style";
+import Header from "../../components/Header";
 
 const DiagnosesIntakeScreen = ({ navigation }) => {
     return (
-        <LinearGradient>
+        <LinearGradient colors={Gradients.gradient1} style={{flex: 1}}>
             <SafeAreaView>
                 <View>
-                    <Header />
+                    <Header navigation={navigation} />
                 </View>
             </SafeAreaView>
         </LinearGradient>
     );
 };
+
+export default DiagnosesIntakeScreen;

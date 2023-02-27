3451754181a82d075ad00db643afff39ee855cf0
fossjb <64759316+fossjb@users.noreply.github.com>
2023-02-27 13:15:06 -0500

    Update DiagnosesIntakeScreen.js
    Finished baseline of DiagnosesIntakeScreen.js

--- wallet-mobile/src/ui/views/intake/DiagnosesIntakeScreen.js
+++ wallet-mobile/src/ui/views/intake/DiagnosesIntakeScreen.js
@@ -1,37 +1,57 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, TextInput, StyleSheet } from "react-native";
 import { Gradients, TextStyles, ScreenStyles } from "../../Style";
 import Header from "../../components/Header";
 import CheckboxList from "../../components/CheckboxList";
+import { PrimaryButton } from "../../components/Buttons";
 
 const DiagnosesIntakeScreen = ({ navigation }) => {
   return (
-    <LinearGradient colors={Gradients.gradient1} style={{ flex: 1 }}>
-      <SafeAreaView>
+    <LinearGradient
+      colors={Gradients.gradient1}
+      style={{ flex: 1, height: "100%" }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
         <Header navigation={navigation} />
-        <View style={{ paddingHorizontal: 30 }}>
+        <View style={{ paddingHorizontal: 30, flex: 1, justifyContent: "space-between" }}>
+          <View>
           <Text style={TextStyles.header}>Diagnoses</Text>
-          <Text style={TextStyles.description}>
-            Do you have any pre-existing diagnoses?
-          </Text>
-          <Text style={TextStyles.description}>Select all that apply.</Text>
-          <View style={{ marginVertical: 10 }}></View>
-          <View
-            style={ScreenStyles.section}
-          >
-            <CheckboxList
-              options={[
-                "Condition 1",
-                "Condition 2",
-                "Condition 3",
-                "Condition 4",
-              ]}
-            />
+            <Text style={TextStyles.description}>
+              Do you have any pre-existing diagnoses?
+            </Text>
+            <Text style={TextStyles.description}>Select all that apply.</Text>
+            <View style={{ marginVertical: 10 }}></View>
+            <View style={ScreenStyles.section}>
+              <CheckboxList
+                options={[
+                  "Condition 1",
+                  "Condition 2",
+                  "Condition 3",
+                  "Condition 4",
+                ]}
+              />
+              <View style={{ marginVertical: 10 }}></View>
+              <Text style={TextStyles.description}>Other</Text>
+              <TextInput style={styles.input} placeholder="Condition 5" />
+            </View>
+          </View>
+          <View>
+            <PrimaryButton label={"Save"} />
           </View>
         </View>
       </SafeAreaView>
     </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    color: "#EEE",
+    borderBottomColor: "#EEE",
+    borderBottomWidth: 1,
+    fontSize: 20,
+    paddingVertical: 5,
+  },
+});
 
 export default DiagnosesIntakeScreen;

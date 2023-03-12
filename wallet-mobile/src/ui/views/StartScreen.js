79b8fb41b2e8876343f99c8e579508562ed89818
fossjb <64759316+fossjb@users.noreply.github.com>
2023-03-12 13:05:32 -0400

    Update StartScreen.js Responsive

--- wallet-mobile/src/ui/views/StartScreen.js
+++ wallet-mobile/src/ui/views/StartScreen.js
@@ -1,100 +1,62 @@
-import React from "react";
-import {
-    View,
-    Text,
-    StyleSheet,
-    Dimensions
-} from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { ScreenStyles, Gradients } from "../Style";
+import { Gradients } from "../Style";
 import { LargeLogo } from "../components/Logos";
 import { PrimaryButton } from "../components/Buttons";
+import { Flex, Box, VStack, HStack, Text, Button } from "native-base";
 
 const StartScreen = ({ navigation }) => {
-
-    const renderLoginLink = () => {
-        const styles = StyleSheet.create({
-            container: {
-                width: '100%',
-                height: 32,
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-            },
-            message: {
-                color: '#eeeeee'
-            },
-            span: {
-                paddingLeft: 8,
-                color: '#2a93d5'
-            }
-        });
-        return (
-            <View style={styles.container}>
-                <Text style={styles.message}>Already have an account?</Text>
-                <Text style={styles.span} onPress={() => {
-                    return navigation.navigate("Login");
-                }}>
-                    Log In
-                </Text>
-            </View>
-        );
-    }
-
-    return (
-        <View style={ScreenStyles.screen}>
-            <LinearGradient
-                colors={Gradients.gradient1}
-                style={ScreenStyles.gradient}
+  return (
+    // Page Container
+    <Flex
+      flex="1"
+      justifyContent="center"
+      alignItems="center"
+      bg={{
+        linearGradient: {
+          colors: Gradients.gradient1,
+        },
+      }}
+    >
+      {/* Screen Container */}
+      <VStack
+        flex="1"
+        maxWidth="400"
+        safeArea
+        alignItems="center"
+        justifyContent="space-evenly"
+        p="5"
+      >
+        {/* Logo Container */}
+        <Box flex="1" justifyContent="center">
+          <LargeLogo />
+        </Box>
+        {/* Button / Text Container */}
+        <VStack space="2">
+          <Button
+            variant="outline"
+            onPress={() => {
+              return navigation.navigate("Splash");
+            }}
+            _text={{color: "#EEE", fontSize: "lg"}}
+            rounded="7"
+          >
+            Get Started
+          </Button>
+          <HStack justifyContent="center" alignItems="center" space="1">
+            <Text color="#EEE" fontSize="sm">Already have an account?</Text>
+            <Text
+              color="#2A93D5"
+              fontSize="sm"
+              onPress={() => {
+                return navigation.navigate("Login");
+              }}
             >
-                <View style={StartStyles.container}>
-                    <View style={StartStyles.banner}>
-                        <LargeLogo />
-                    </View>
-                    <View style={StartStyles.jumbotron}>
-                        <Text style={StartStyles.message}></Text>
-                    </View>
-                    <View style={StartStyles.form}>
-                        <PrimaryButton label="Get Started" options={{
-                            onPress: () => {
-                                return navigation.navigate('Splash');
-                            }
-                        }}/>
-                        {renderLoginLink()}
-                    </View>
-                </View>
-            </LinearGradient>
-        </View>
-    );
+              Log In
+            </Text>
+          </HStack>
+        </VStack>
+      </VStack>
+    </Flex>
+  );
 };
-
-const StartStyles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: '100%',
-        padding: 16,
-        flexDirection: 'column',
-    },
-    banner: {
-        flex: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    jumbotron: {
-        flex: 4,
-    },
-    message: {
-        fontSize: 50,
-        color: "#ffffff"
-    },
-    form: {
-        flex: 1,
-        width: '100%',
-        paddingVertical: 16,
-        paddingHorizontal: 32,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
 
 export default StartScreen;

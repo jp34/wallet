0d2b523a2a7d4731401c850574ea6737ec9dcaba
fossjb <64759316+fossjb@users.noreply.github.com>
2023-03-13 08:08:39 -0400

    Finish Agreement.js Form Validation / Responsive

--- wallet-mobile/src/ui/views/signup/AgreementScreen.js
+++ wallet-mobile/src/ui/views/signup/AgreementScreen.js
@@ -1,106 +1,105 @@
 import React from "react";
 import {
-    View,
-    Text,
-    TextInput,
-    StyleSheet,
-} from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { PrimaryButton } from "../../components/Buttons";
-import { ScreenStyles, Gradients } from "../../Style";
-import Checkbox from "expo-checkbox";
-import Header from "../../components/Header";
+  Flex,
+  Container,
+  Pressable,
+  ChevronLeftIcon,
+  Text,
+  Box,
+  ScrollView,
+  Checkbox,
+  Button,
+} from "native-base";
 
 const AgreementScreen = ({ navigation }) => {
+  const [confirm, setConfirm] = React.useState(false);
 
-    const [confirm, setConfirm] = React.useState(false);
-
-    const renderAgreement = () => {
-        const styles = StyleSheet.create({
-            container: {
-                width: '100%',
-            },
-            text: {
-                paddingVertical: 16,
-                paddingHorizontal: 16,
-                backgroundColor: '#eeeeee',
-                borderWidth: 1,
-                borderColor: '#cccccc',
-                borderRadius: 8,
-            },
-            title: {
-                marginVertical: 16,
-                color: '#eeeeee',
-                fontSize: 24,
-                fontFamily: 'Quicksand-Regular',
-            },
-            confirmContainer: {
-                paddingVertical: 16,
-                paddingHorizontal: 8,
-                flexDirection: 'row',
-                alignItems: 'center',
-            },
-            confirm: {
-                width: 15,
-                height: 15,
-                margin: 4,
-            },
-            confirmLabel: {
-                color: '#eeeeee',
-                fontSize: 18,
-                fontFamily: 'Quicksand-Regular',
-            },
-        });
-        const options = {
-            style: styles.text,
-            multiline: true,
-            numberOfLines: 15,
-            editable: false,
-            placeholder: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        };
-        return (
-            <View style={styles.container}>
-                <Text style={styles.title}>Terms & Conditions</Text>
-                <TextInput {...options} />
-                <View style={styles.confirmContainer}>
-                    <Checkbox value={confirm} onValueChange={setConfirm} style={styles.confirm}/>
-                    <Text style={styles.confirmLabel}>I agree</Text>
-                </View>
-            </View>
-        );
-    }
-
-    return (
-        <View style={ScreenStyles.screen}>
-            <LinearGradient
-                colors={Gradients.gradient1}
-                style={ScreenStyles.gradient}
-            >
-                <Header navigation={navigation} />
-                <View style={ScreenStyles.container}>
-                    <View style={AgreementStyles.agreement}>
-                        {renderAgreement()}
-                    </View>
-                    <PrimaryButton label="Continue" options={{
-                        onPress: () => {
-                            return navigation.navigate('CreateAccount', { agreement: confirm });
-                        },
-                        disabled: !confirm,
-                    }} />
-                </View>
-            </LinearGradient>
-        </View>
-    );
+  return (
+    // Page Container
+    <Flex
+      flex="1"
+      bg={{
+        linearGradient: {
+          colors: ["violet.900", "violet.600"],
+        },
+      }}
+      alignItems="center"
+      py="3"
+      px="4"
+    >
+      {/* Page Body */}
+      <Container safeArea flex="1" w="full" maxW="390">
+        {/* Navigation Header */}
+        <Pressable
+          onPress={() => navigation.goBack()}
+          flex="0.05"
+          justifyContent="center"
+        >
+          {/* Navigation Icon */}
+          <ChevronLeftIcon color="lightAccent" size="lg" />
+        </Pressable>
+        <Box flex="0.2" justifyContent="center" px="4">
+          <Text fontSize="2xl" color="lightAccent">
+            Terms & Conditions
+          </Text>
+          <Text fontSize="xl" color="lightAccent">
+            Please read and accept the terms.
+          </Text>
+        </Box>
+        <Box
+          flex="0.5"
+          justifyContent="center"
+          alignItems="center"
+          rounded="30"
+          py="3"
+          px="4"
+          shadow="9"
+          bgColor="lightAccent"
+        >
+          <ScrollView>
+            <Text color="purple.800" fontWeight="medium" fontSize="lg">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum."
+            </Text>
+          </ScrollView>
+        </Box>
+        <Box flex="0.15" justifyContent="center" w="full" alignItems="center">
+          <Checkbox
+            size="md"
+            mt="4"
+            onChange={(value) => setConfirm(value)}
+            colorScheme="fuchsia"
+            _text={{color: "lightAccent"}}
+          >
+            I agree to the terms & conditions.
+          </Checkbox>
+        </Box>
+        <Box flex="0.1" w="full" justifyContent="flex-end">
+          <Button
+            variant="outline"
+            colorScheme="white"
+            onPress={() => confirm ? navigation.navigate("CreateAccount") : {} }
+            rounded="7"
+            alignSelf="center"
+            w="70%"
+          >
+            <Text color="#EEE" fontSize="lg">
+              Continue
+            </Text>
+          </Button>
+        </Box>
+      </Container>
+    </Flex>
+  );
 };
-
-const AgreementStyles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        paddingHorizontal: 32,
-    },
-    agreement: {
-        flex: 8,
-    },
-});
 
 export default AgreementScreen;

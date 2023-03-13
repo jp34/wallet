6e118f4afad0b3d57aa32a69f0871e67b821b914
fossjb <64759316+fossjb@users.noreply.github.com>
2023-03-13 06:05:34 -0400

    Changing components for theme.

--- wallet-mobile/src/ui/App.js
+++ wallet-mobile/src/ui/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import Router from "./Router";
-import { NativeBaseProvider} from "native-base";
+import { NativeBaseProvider, extendTheme } from "native-base";
 import { LinearGradient } from "expo-linear-gradient";
 
 const App = () => {
-  const [fonts] = useFonts({
-    "Quicksand-Regular": require("../../assets/fonts/Quicksand-Regular.ttf"),
-    "Quicksand-Medium": require("../../assets/fonts/Quicksand-Medium.ttf"),
-    "Quicksand-SemiBold": require("../../assets/fonts/Quicksand-SemiBold.ttf"),
-    "Quicksand-Bold": require("../../assets/fonts/Quicksand-Bold.ttf"),
-  });
-  if (!fonts) return null;
+  // const [fonts] = useFonts({
+  //   "Quicksand-Regular": require("../../assets/fonts/Quicksand-Regular.ttf"),
+  //   "Quicksand-Medium": require("../../assets/fonts/Quicksand-Medium.ttf"),
+  //   "Quicksand-SemiBold": require("../../assets/fonts/Quicksand-SemiBold.ttf"),
+  //   "Quicksand-Bold": require("../../assets/fonts/Quicksand-Bold.ttf"),
+  // });
+  // if (!fonts) return null;
 
   const config = {
     dependencies: {
@@ -20,8 +19,16 @@ const App = () => {
     },
   };
 
+  const theme = extendTheme({
+    colors: {
+      vibrantAccent: "#6924F5",
+      lightAccent: "#F3E5FF",
+      maybeAccent: "#E324F5"
+    },
+  });
+
   return (
-    <NativeBaseProvider config={config}>
+    <NativeBaseProvider config={config} theme={theme}>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
--- wallet-mobile/src/ui/components/Logos.js
+++ wallet-mobile/src/ui/components/Logos.js
@@ -4,53 +4,53 @@ import { LinearGradient } from "expo-linear-gradient";
 import { Gradients } from "../Style";
 
 export const LargeLogo = () => {
-    const styles = StyleSheet.create({
-        container: {
-            width: 160,
-            aspectRatio:  1,
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 28,
-        },
-        text: {
-            color: "#eeeeee",
-            fontSize: 30,
-            fontWeight: "bold",
-            letterSpacing: 1,
-        },
-    });
-    return (
-        <LinearGradient
-            colors={Gradients.gradient2}
-            style={styles.container}
-        >
-            <Text style={styles.text}>JustBe</Text>
-        </LinearGradient>
-    );
-}
+  const styles = StyleSheet.create({
+    container: {
+      width: 160,
+      aspectRatio: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 28,
+    },
+    text: {
+      color: "#eeeeee",
+      fontSize: 30,
+      fontWeight: "bold",
+      letterSpacing: 1,
+    },
+  });
+  return (
+    <LinearGradient
+      colors={["#6d28d9", "#4c1d95"]}
+      style={styles.container}
+    >
+      <Text style={styles.text}>JustBe</Text>
+    </LinearGradient>
+  );
+};
 
 export const SmallLogo = () => {
-    const styles = StyleSheet.create({
-        container: {
-            width: 55,
-            aspectRatio:  1,
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 10,
-        },
-        text: {
-            color: "#eeeeee",
-            fontSize: 12,
-            fontWeight: "bold",
-            letterSpacing: 1,
-        },
-    });
-    return (
-        <LinearGradient
-            colors={Gradients.gradient2}
-            style={styles.container}
-        >
-            <Text style={styles.text}>JustBe</Text>
-        </LinearGradient>
-    );
-}
+  const styles = StyleSheet.create({
+    container: {
+      width: 55,
+      aspectRatio: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+    },
+    text: {
+      color: "#eeeeee",
+      fontSize: 12,
+      fontWeight: "bold",
+      letterSpacing: 1,
+    },
+  });
+  return (
+    <LinearGradient
+      colors={["violet.600", "violet.900"]}
+      style={styles.container}
+    >
+      <Text style={styles.text}>JustBe</Text>
+    </LinearGradient>
+  );
+};

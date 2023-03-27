b1ecef27da885f7f3a557f59f75faeb274f8694c
fossjb <64759316+fossjb@users.noreply.github.com>
2023-03-27 08:26:37 -0400

    Recover accidentally deleted file

---
+++ wallet-mobile/src/ui/views/StartScreen.js
@@ -0,0 +1,67 @@
+import { Link } from "expo-router";
+import {
+  Center,
+  VStack,
+  HStack,
+  Text,
+  Button,
+  Pressable,
+  Box,
+} from "native-base";
+import { Logo } from "../../components/Logo";
+import Wrapper from "../../components/Wrapper";
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Wrapper>
+      <Center flex="0.9">
+        <Logo lg />
+      </Center>
+      <Box flex="0.1" alignSelf="center">
+        {renderButtonFooter()}
+      </Box>
+    </Wrapper>
+  );
+
+  function renderButtonFooter() {
+    return (
+      <VStack space="2">
+        <Link href="splash" asChild>
+          <Button
+            variant="primary"
+            _text={{
+              fontSize: "lg",
+            }}
+          >
+            Get Started
+          </Button>
+        </Link>
+        <HStack space="2">
+          <Text
+            color="#EEE"
+            fontSize={{
+              base: "md",
+              sm: "sm",
+              xl: "lg",
+            }}
+          >
+            Already have an account?
+          </Text>
+          <Pressable onPress={() => navigation.navigate("Login")}>
+            <Text
+              color="secondaryBlue.600"
+              fontSize={{
+                base: "md",
+                sm: "sm",
+                xl: "lg",
+              }}
+              fontWeight="semibold"
+            >
+              Log In
+            </Text>
+          </Pressable>
+        </HStack>
+      </VStack>
+    );
+  }
+}

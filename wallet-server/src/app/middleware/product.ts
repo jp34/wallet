95af5dc2a0e66093195f9fc95059bd3b2b8a3849
jp34 <joekadlic@outlook.com>
2023-05-02 13:27:07 -0400

    Made voucher creation optional

--- wallet-server/src/app/middleware/product.ts
+++ wallet-server/src/app/middleware/product.ts
@@ -1,13 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import { createProduct } from "../../service/product/product.service";
 import logger from "../../config/logger";
+import ProductFactory from "../product/ProductFactory";
 
 export const syncProduct = async (request: Request, response: Response, next: NextFunction) => {
     logger.info("here");
     const id = parseInt(request.params.id);
     if (!id) throw new Error("Unable to retrieve id from url params");
     if (response.statusCode != 200) return next();
-    logger.info(`Deteced change to patient model: Patient(${id})`);
-    createProduct(id);
+    logger.info(`Generating new product for patient: ${id}`);
+    new ProductFactory().generate(id);
     next();
 }
--- wallet-server/src/service/product/product.service.ts
+++ wallet-server/src/service/product/product.service.ts
@@ -55,54 +55,6 @@ export const productExistsByCid = async (cid: string) => {
     return (count > 0);
 }
 
-const createVoucher = async (cid: string) => {
-    const contractHandler: ContractHandler = new ContractHandler();
-    const uri = 'ipfs://' + cid;
-    const price: ethers.BigNumberish = ethers.parseEther('1');
-    return await contractHandler.makeVoucher(cid, uri, price);
-}
-
-export const createProduct = async (id: number) => {
-    try {
-        // Fetch patient data
-        const patient = await prisma.patient.findUnique({
-            where: {
-                id: id
-            }
-        });
-        if (!patient) throw new Error(`Patient does not exist with id: ${id}`);
-        logger.info(`Generating new EMR for patient: ${patient.id}`);
-
-        // Send patient data to Web3-Listener
-        const cid = await upload([{
-            meta: {
-                subject: patient.id
-            },
-            data: patient
-        }]);
-
-        const exists = await productExistsByCid(cid);
-        if (exists) return;
-
-        // Create NFT voucher
-        const voucher = await createVoucher(cid);
-
-        // Store new product
-        const receipt = await prisma.product.create({
-            data: {
-                cid: cid,
-                subjectId: patient.id,
-                voucher: JSON.stringify(voucher)
-            }
-        });
-        if (!receipt) throw new Error("Unable to store EMR transaction receipt");
-        return true;
-    } catch (err: any) {
-        logger.warn(`EMR generation for Patient(${id}) produced error: ${err.message}`);
-        throw err;
-    }
-}
-
 export const purchaseProduct = async (id: number, buyer: string) => {
     // Mark product as purchased
     const product = await prisma.product.update({
